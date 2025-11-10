9a2176e93d81df36d5b6b5c8a95e870a35a30e13
jpadilla535123-crypto <j.padilla.535123@umindanao.edu.ph>
2025-11-10 15:20:33 +0800

    laban

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,27 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->name('dashboard');
 
 Route::get('/properties', function () {
     return view('properties');
-});
+})->name('properties');
 
 Route::get('/smartdevices', function () {
     return view('smartdevices');
-});
+})->name('smartdevices');
 
 Route::get('/tenants', function () {
     return view('tenants');
-});
+})->name('tenants');
 
 Route::get('/maintenance', function () {
     return view('maintenance');
-});
+})->name('maintenance');
+
+Route::get('/analytics', function () {
+    return view('analytics');
+})->name('analytics');
 
 Route::get('/login', function () {
     return view('login');
